d66c8e20a9916587ed7f376eafd2309369b61b96
Mohit Kumar Singh <128493206+mohitkumar0502@users.noreply.github.com>
2023-03-28 19:59:59 +0530

    Add files via upload
    swap with predefine

---
+++ USwap.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class USwap {
+public static void main(String arg[])
+{ 
+int a=2;int b=4;
+int c;
+System.out.println("Before Swapping="+a+" "+b);
+c=a;
+a=b;
+b=c;
+System.out.println("After Swapping="+a+" "+b);
+}
+}
+
